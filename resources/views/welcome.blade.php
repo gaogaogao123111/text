3afd9108b1e6cdc48e0830ee4daa65e2bce49471
gxd <17610531571@163.com>
2019-04-26 03:35:42 +0000

    a

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    aaLaravel
+                    textLaravel
                 </div>
 
                 <div class="links">
